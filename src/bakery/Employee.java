5ac45ae1a8d501ff9a53c404b748c78b9b3b7d9a
Vladimir Zorov <vl.zorov@gmail.com>
2022-12-26 14:21:14 +0200

    add

---
+++ src/bakery/Employee.java
@@ -0,0 +1,19 @@
+package bakery;
+
+public class Employee {
+
+    private String name;
+    private int age;
+    private String country;
+
+    public Employee(String name, int age, String country) {
+        this.name = name;
+        this.age = age;
+        this.country = country;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Employee: %s, %d (%s)", name, age, country);
+    }
+}
